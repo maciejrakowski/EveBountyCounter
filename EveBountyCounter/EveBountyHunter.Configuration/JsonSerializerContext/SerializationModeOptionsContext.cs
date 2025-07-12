1edb16aa2972c6c4251cc3cbf9e60c9764676004
Maciej Rakowski <m.rakoo@gmail.com>
2025-07-12 19:28:51 +0200

    Potential fix fro serialization issue

---
+++ EveBountyCounter/EveBountyHunter.Configuration/JsonSerializerContext/SerializationModeOptionsContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using EveBountyHunter.Configuration.Models;
+
+namespace EveBountyHunter.Configuration.JsonSerializerContext;
+
+[JsonSourceGenerationOptions(WriteIndented = true, PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, GenerationMode = JsonSourceGenerationMode.Serialization)]
+[JsonSerializable(typeof(EveBountyCounterConfiguration))]
+internal partial class SerializationModeOptionsContext : System.Text.Json.Serialization.JsonSerializerContext
+{
+}
